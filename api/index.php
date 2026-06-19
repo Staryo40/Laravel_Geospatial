fb07662432a716fbcda9985fe4b1150cc54a5198
Staryo40 <arrrryow@gmail.com>
2026-06-19 23:15:34 +0700

    fix: override on laravel several environment variables

--- api/index.php
+++ api/index.php
@@ -4,21 +4,30 @@ use Illuminate\Http\Request;
 
 define('LARAVEL_START', microtime(true));
 
-// Determine if the application is in maintenance mode...
 if (file_exists($maintenance = __DIR__.'/../storage/framework/maintenance.php')) {
     require $maintenance;
 }
 
-// Register the Composer autoloader...
 require __DIR__.'/../vendor/autoload.php';
 
-// Ensure storage folders exist in /tmp for Vercel's read-only environment
 foreach (['/tmp/storage/framework/views', '/tmp/storage/framework/cache', '/tmp/storage/framework/sessions'] as $path) {
     if (!is_dir($path)) {
         @mkdir($path, 0755, true);
     }
 }
 
-// Bootstrap Laravel and handle the request...
+foreach ([
+    'APP_SERVICES_CACHE' => '/tmp/storage/framework/cache/services.php',
+    'APP_PACKAGES_CACHE' => '/tmp/storage/framework/cache/packages.php',
+    'APP_CONFIG_CACHE' => '/tmp/storage/framework/cache/config.php',
+    'APP_ROUTES_CACHE' => '/tmp/storage/framework/cache/routes.php',
+    'APP_EVENTS_CACHE' => '/tmp/storage/framework/cache/events.php',
+    'VIEW_COMPILED_PATH' => '/tmp/storage/framework/views',
+] as $key => $val) {
+    putenv("{$key}={$val}");
+    $_ENV[$key] = $val;
+    $_SERVER[$key] = $val;
+}
+
 (require_once __DIR__.'/../bootstrap/app.php')
     ->handleRequest(Request::capture());
